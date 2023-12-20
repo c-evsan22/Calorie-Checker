93179e018aef3cd2fd62d8b02fe614631731eb62
Chris San <christianes322@gmail.com>
2023-12-20 14:49:31 -0500

    finished complete framework

---
+++ config/config.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+
+module.exports = {
+    development: {
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        dialect: "mysql",
+        apiKey: process.env.API_KEY
+    },
+    test: {
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        dialect: "mysql",
+        apiKey: process.env.API_KEY
+    },
+    production: {
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        dialect: "mysql",
+        apiKey: process.env.API_KEY
+    }
+}
--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,39 @@
-module.exports = function(req, res, next) {
-    if (req.user) {
-        return next();
-    }
-    return res.redirect("/login");
+var passport = require("passport");
+var LocalStrategy = require("passport-local").Strategy;
 
-};
+var db = require("../models");
+
+passport.use(new LocalStrategy(
+    {
+        usernameField: "username"
+    },
+    function(username, password, done) {
+        db.User.findOne({
+            where: {
+                username: username
+            }
+        }).then(function(dbUser) {
+            if (!dbUser) {
+                return done(null, false, {
+                    message: "Incorrect username."
+                });
+            }
+            else if (!dbUser.validPassword(password)) {
+                return done(null, false, {
+                    message: "Incorrect password."
+                });
+            }
+            return done(null, dbUser);
+        });
+    }
+));
+
+passport.serializeUser(function(user, cb) {
+    cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+    cb(null, obj);
+});
+
+module.exports = passport;
